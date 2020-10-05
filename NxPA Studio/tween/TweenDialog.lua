f6213961f955c723d3835157bfd42e8f31554014
Kacper Woźniak <kac.wozniak@gmail.com>
2020-10-05 22:47:14 +0100

    Remove option to loop in Tweener and add ability to only interpolate frames in a selected range

--- NxPA Studio/tween/TweenDialog.lua
+++ NxPA Studio/tween/TweenDialog.lua
@@ -1,23 +1,45 @@
 Tweener = dofile("./Tweener.lua");
 
+function getFirstAndLastFrameNumber(sprite)
+    if app.range.isEmpty then return {first = 1, last = #sprite.frames} end
+
+    local first = #sprite.frames;
+    local last = 1;
+
+    for _, frame in ipairs(app.range.frames) do
+        local frameNumber = frame.frameNumber;
+        if frameNumber < first then first = frameNumber; end
+        if frameNumber > last then last = frameNumber; end
+    end
+
+    return {first = first, last = last}
+end
+
 return function(dialogTitle)
     local dialog = Dialog(dialogTitle);
 
+    local frameNumbers = getFirstAndLastFrameNumber(app.activeSprite)
+
     dialog:number{
-        id = "frames",
-        label = "Frames:",
-        text = "2",
-        decimals = false
-    }:button{
-        text = "Tween",
-        onclick = function()
-            Tweener:tween{
-                sprite = app.activeSprite,
-                loop = dialog.data["loop"],
-                frames = dialog.data["frames"]
-            }
-        end
-    }:check{id = "loop", label = "Loop:", selected = false}
+        id = "firstFrame",
+        label = "From frame",
+        text = tostring(frameNumbers.first)
+    }:number{
+        id = "lastFrame",
+        label = "To frame",
+        text = tostring(frameNumbers.last)
+    }:number{id = "framesToAdd", label = "Number of frames to add:", text = "1"}
+        :button{
+            text = "Tween",
+            onclick = function()
+                Tweener:tween{
+                    sprite = app.activeSprite,
+                    firstFrame = dialog.data["firstFrame"],
+                    lastFrame = dialog.data["lastFrame"],
+                    framesToAdd = dialog.data["framesToAdd"]
+                }
+            end
+        }
 
     return dialog;
 end
--- NxPA Studio/tween/Tweener.lua
+++ NxPA Studio/tween/Tweener.lua
@@ -1,51 +1,72 @@
-local Tweener = {}
+local Tweener = {isDebug = false}
 
 function Tweener:tween(config)
-    if not config or not config["sprite"] or not config["frames"] then return end
+    if not config or not config["sprite"] or not config["framesToAdd"] then
+        return
+    end
 
     app.transaction(function()
         local sprite = config["sprite"]
-        local loop = config["loop"]
-        local numberOfFramesToAdd = config["frames"]
+        local firstFrame = config["firstFrame"]
+        local lastFrame = config["lastFrame"]
+        local numberOfFramesToAdd = config["framesToAdd"]
 
-        self:addInbetweenFrames(sprite, numberOfFramesToAdd, loop)
-        self:moveInbetweenFrames(sprite, numberOfFramesToAdd + 1, loop)
+        local lastFrameToMove = lastFrame + (lastFrame - firstFrame) *
+                                    numberOfFramesToAdd
+
+        self:addInbetweenFrames(sprite, firstFrame, lastFrame,
+                                numberOfFramesToAdd)
+        self:moveInbetweenFrames(sprite, firstFrame, lastFrameToMove,
+                                 numberOfFramesToAdd)
     end)
 end
 
-function Tweener:addInbetweenFrames(sprite, numberOfFramesToAdd, loop)
-    local numberOfFrames = #sprite.frames
-    local numberOfAddedFrames = 0
-
-    local firstFrameIndex = 1
-    local numberOfFramesToClone = loop and numberOfFrames or numberOfFrames - 1
-
+function Tweener:addInbetweenFrames(sprite, firstFrame, lastFrame,
+                                    numberOfFramesToAdd)
     -- Add inbetween frames after all frames except for the last one
-    for i = firstFrameIndex, numberOfFramesToClone do
-        local frameToClone = i + (i - firstFrameIndex) * numberOfFramesToAdd
+    for i = firstFrame, lastFrame - 1 do
+        local frameToClone = i + (i - firstFrame) * numberOfFramesToAdd
 
         for j = 1, numberOfFramesToAdd do sprite:newFrame(frameToClone) end
     end
 end
 
-function Tweener:moveInbetweenFrames(sprite, originalNumberOfFrames, loop)
+function Tweener:moveInbetweenFrames(sprite, firstFrame, lastFrame,
+                                     numberOfFramesToAdd)
     local stepX = 0
     local stepY = 0
 
-    for i, layer in ipairs(sprite.layers) do
-        for j, cel in ipairs(layer.cels) do
-            local step = (j - 1) % originalNumberOfFrames
+    local delta = numberOfFramesToAdd + 1
+
+    self:log("Delta " .. tostring(delta))
+
+    self:log("Processing frames from " .. tostring(firstFrame) .. " to " ..
+                 tostring(lastFrame))
+
+    for layerNumber, layer in ipairs(sprite.layers) do
+        self:log("Processing layer " .. tostring(layerNumber) .. "...")
+
+        for _, cel in ipairs(layer.cels) do
+            local celNumber = cel.frameNumber
+
+            if celNumber < firstFrame or celNumber >= lastFrame then
+                goto continue
+            end
+
+            self:log("Processing cel " .. tostring(celNumber) .. "")
+
+            local step = (celNumber - firstFrame) % delta
+
+            self:log("Step " .. tostring(step))
 
             if step == 0 then
-                local next = layer.cels[j + originalNumberOfFrames]
+                self:log("Next Original Cel " .. tostring(celNumber + delta))
 
-                if loop and not next then next = layer.cels[1] end
+                local next = layer.cels[celNumber + delta]
 
                 if next then
-                    stepX = (next.position.x - cel.position.x) /
-                                originalNumberOfFrames
-                    stepY = (next.position.y - cel.position.y) /
-                                originalNumberOfFrames
+                    stepX = (next.position.x - cel.position.x) / delta
+                    stepY = (next.position.y - cel.position.y) / delta
                 end
             else
                 cel.position = {
@@ -53,8 +74,14 @@ function Tweener:moveInbetweenFrames(sprite, originalNumberOfFrames, loop)
                     y = cel.position.y + math.floor(stepY * step)
                 }
             end
+
+            ::continue::
         end
+
+        self:log(" ")
     end
 end
+
+function Tweener:log(message) if self.isDebug then print(message) end end
 
 return Tweener;
