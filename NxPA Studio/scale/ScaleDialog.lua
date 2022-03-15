9c4b0336e2c71f66b30cffb30608e3d1d1ccfbb5
Kacper Woźniak <kac.wozniak@gmail.com>
2022-03-15 20:24:28 +0100

    Disable buttons for scaling when there's no active sprite

--- NxPA Studio/scale/ScaleDialog.lua
+++ NxPA Studio/scale/ScaleDialog.lua
@@ -2,11 +2,26 @@ Transaction = dofile("../shared/Transaction.lua");
 ScaleAlgorithm = dofile("./ScaleAlgorithm.lua");
 
 return function(dialogTitle)
-    local dialog = Dialog(dialogTitle)
+    -- Declare a variable for dialog to reference in "onSiteChange" event handler
+    local dialog = nil
+
+    local function onSiteChange()
+        dialog --
+        :modify{id = "eagle", enabled = app.activeSprite ~= nil} --
+        :modify{id = "scale2x", enabled = app.activeSprite ~= nil} --
+        :modify{id = "scale3x", enabled = app.activeSprite ~= nil}
+    end
+    app.events:on('sitechange', onSiteChange)
+
+    dialog = Dialog {
+        title = dialogTitle,
+        onclose = function() app.events:off(onSiteChange) end
+    }
 
     dialog --
     :separator{text = "Algorithm"} --
     :button{
+        id = "eagle",
         text = "Eagle",
         onclick = Transaction(function()
             ScaleAlgorithm:Eagle(app.activeSprite)
@@ -14,12 +29,14 @@ return function(dialogTitle)
     } --
     :newrow() --
     :button{
+        id = "scale2x",
         text = "Scale2x",
         onclick = Transaction(function()
             ScaleAlgorithm:Scale2x(app.activeSprite)
         end)
     } --
     :button{
+        id = "scale3x",
         text = "Scale3x",
         onclick = Transaction(function()
             ScaleAlgorithm:Scale3x(app.activeSprite)
