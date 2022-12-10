ea35a402c2d5627be71a8df13ba2c0aa600c017f
Kacper Woźniak <kac.wozniak@gmail.com>
2022-12-10 15:39:48 +0100

    Fix extension packing script to not include README images and duplicate files

--- PackExtension.ps1
+++ PackExtension.ps1
@@ -3,9 +3,9 @@ param ($Path)
 $Package = Get-Content (Join-Path -Path $Path -ChildPath "package.json") | Out-String | ConvertFrom-Json
 $PackageName = "$($Package.displayName) v$($Package.version)"
 
-$ArchiveFiles = Get-ChildItem -Recurse -Name -Path $Path -Exclude "*.md", "*.zip", "*.ps1", "*.itch-toml", "*.aseprite-extension", "\readme-images\*", "*.gif" | ForEach-Object{".\$Path\$_"}
+$ArchiveFiles = Get-ChildItem -Name -Path $Path -Exclude "*.md", "*.zip", "*.ps1", "*.itch-toml", "*.aseprite-extension", "readme-images", "*.gif"  | ForEach-Object { ".\$Path\$_" }
 
-Compress-Archive -Path $ArchiveFiles -DestinationPath ".\Output\$PackageName.zip" -Update
+Compress-Archive -Path $ArchiveFiles -DestinationPath ".\Output\$PackageName.zip"
 
 if (Test-Path -Path ".\Output\$PackageName.aseprite-extension") {
     Remove-Item -Path ".\Output\$PackageName.aseprite-extension"
