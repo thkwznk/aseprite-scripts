dabdcf14d372d9f775382c2e03c6e3cfc1226fb4
Kacper Woźniak <kac.wozniak@gmail.com>
2023-02-02 22:33:05 +0100

    Move initializing default themes into the ThemeManager

--- Theme Preferences/extension.lua
+++ Theme Preferences/extension.lua
@@ -624,32 +624,10 @@ function init(plugin)
     -- Do nothing when UI is not available
     if not app.isUIAvailable then return end
 
-    -- TODO: v Move this to the ThemeManager
-    -- Initialize a table in preferences to persist data
+    -- Initialize plugin preferences data for backwards compatibility
     plugin.preferences.themePreferences =
-        plugin.preferences.themePreferences or {
-            savedThemes = {
-                "<Peach:1:A:////6MbGnnx8eGBQ////98vfxpKerlVh/+u2////0sq9lYF0ZFVgAgIC/f39/0wA/0wA/1dX////xsbGtbW1ZVVhQUEs/wB9mwBdggAf/v//f39/AQAAAQEB>",
-                "<Garden:1:B:////3u7T/5wAaseC7f//8cq9aseCUnNh/+u2////8cq9tIF0g1VgAAAA////ATskATsk/5wA////xsbGtbW1YGBgPEwr/5wAm0IAggQA/v///s1//5wAATsk>",
-                "<Blue:1:A:////5ubmnJyceGBQ6O//lrr/ZYHNTUSQ/+u26fL+vLy8f3NzTkdfAAAA////S1r/S1r/AM3/6fL+sLnFn6i0TExMKDgXAM3/AHPfADWhAAAAcH9/4P//AAFV>",
-                "<Professional:1:A:////4ODglpaW/4AA////0dnhoKCgiGNj4ODg7fb/wMDAg3d3UktjICAg////QEBAQEBA/4AA7fb/tL3Go6y1QEBAHCwL/4AAmyYAggAA////f39/AAAAICAg>",
-                "<Eva:1:B:////zc3Ng4OD/74t/9H/rpzffWOeZSZh/74tkOqAwMDAOm5ZCUJFAAAA////kOqAOm5ZOm5ZkOqAV7FHRqA2XFxcOEgnkOqALJBgE1IiAAAASHVAkOqAAQEB>",
-                "<Orange:1:B:////xsbG/4AAeGBQ/8eR/4AAaGhodysr/+u2////0sq9lYF0ZFVgAgIC/f39/1cA/1cA/1cA////xsbGtbW1UFBQLDwb/8eRm21xgi8z/4AA/79/////AQEB>",
-                "<Magic:1:B:////zMzMMbnBAICAg+7iMbnBAICAAENDoKCg7fb/wMDAAICAAFRsAgIC/f39/wD//wD//wD/7fb/tL3Go6y1YGBgPEwr/wD/mwDfggCh/v///n///wD/AQEB>",
-                "<Game Boy:1:B:4PjQiMBwNGhWNGhW4PjQiMBwNGhWCBgg4PjQ4PjQiMBwNGhWAzxCCBgg4PjQ4PjQNGhWNGhW4PjQp7+Xlq6GCBggAAQA4PjQfJ6wY2By4PjQdIh4CBggCBgg>",
-                "<Warm:1:A:///jz8aqhXxgeGBQ//jezcO9nIp8hE0//+u2////0sq9lYF0ZFVgAgIC/f39QW4ZQW4Z/3sk////xsbGtbW1XFxcOEgn//99fZKdgmcf/v//n7aMQW4ZAQEB>",
-                "<Dark:1:B:iYmJYWFhFxcXEiE7KXf/AEnHMzMzEiE7AEnHYGBgUFBQICAgAAAMwMDA////ALTmALTmKXf/YGBgJycnFhYWAAAAAAAAKXf/AB3fAACh////lLv/KXf/AAAA>",
-                "<Mono Light:1:B:3d3dzMzMAAAA////7u7u3d3dzMzMAAAA////3d3dzMzMAAAAAAAAAAAA////////////AAAA3d3dpKSkk5OTzMzMqLiX////m6Xfgmeh/v//f39/AQAAAAAA>",
-                "<Mono Dark:1:B:UFBQQEBAAAAAAAAAYGBgUFBQQEBAAAAAMDAwUFBQQEBAAAAAAAAAxMTE////////////AAAAUFBQFxcXBgYGQEBAHCwLAAAAAAAAAAAAQEBAn5+f////AAAA>",
-                "<Game Boy Light:1:B:7//qrtnISLGWSLGW7//qrtnISLGWCQkIrtnI7//q7//qSLGWF4WCCQkI7//qrtnISLGWSLGW7//qtsaxpbWgrtnIisWTrtnISn+oMUFq7//qfIR5CQkICQkI>"
-            },
-            currentTheme = "<Default:1:A:////xsbGfHx8eGBQ////rsvffZKeZVVh/+u2////0sq9lYF0ZFVgAgIC/f39LEyRLEyR/1dX////xsbGtbW1ZVVhQUEs//99m6Vdgmcf/v//e3x8AQAAAQEB>"
-        }
-
+        plugin.preferences.themePreferences or {}
     local storage = plugin.preferences.themePreferences
-
-    -- Initialize a table in prefereces to save themes
-    if storage.savedThemes == nil then storage.savedThemes = {} end
 
     ThemeManager:Init{storage = storage}
     FontsProvider:Init{storage = storage}
