46c81632487b5817eaf04ecc64ffcefc6ee188ef
Kacper Woźniak <kac.wozniak@gmail.com>
2023-02-18 19:02:47 +0100

    Only show anchors if tracked cels have variable size

--- Micro Extensions/Cel Tracking/extension.lua
+++ Micro Extensions/Cel Tracking/extension.lua
@@ -104,60 +104,104 @@ function init(plugin)
 
             local framesOptions = GetFramesOptions(sprite)
 
+            local getSelectedFramesRange = function()
+                local framesOption = dialog.data["framesOption"]
+
+                local framesRange = {fromFrame = 1, toFrame = #sprite.frames}
+
+                if framesOption == AllFrames then
+                    -- Nothing specific
+                elseif framesOption == SpecificFrames then
+                    -- TODO: Implement
+                else -- Tag
+                    local tagName = string.sub(framesOption,
+                                               #TagFramesPrefix + 1)
+
+                    for _, tag in ipairs(sprite.tags) do
+                        if tag.name == tagName then
+                            framesRange = {
+                                fromFrame = tag.fromFrame.frameNumber,
+                                toFrame = tag.toFrame.frameNumber
+                            }
+                            break
+                        end
+                    end
+                end
+
+                return framesRange
+            end
+
+            local updateAnchors = function()
+                local trackedLayer = layers[dialog.data.trackedLayer]
+                local framesRange = getSelectedFramesRange()
+
+                local visible = false
+                local startBounds = nil
+
+                -- If cels change size, show anchor options
+                for frameNumber = framesRange.fromFrame, framesRange.toFrame do
+                    local cel = trackedLayer:cel(frameNumber)
+
+                    if cel then
+                        if startBounds == nil then
+                            startBounds = cel.bounds
+                        end
+
+                        if startBounds.width ~= cel.bounds.width or
+                            startBounds.height ~= cel.bounds.height then
+                            visible = true
+                            break
+                        end
+                    end
+                end
+
+                dialog --
+                :modify{id = "anchor-separator", visible = visible} --
+                :modify{id = "top-left-anchor", visible = visible} --
+                :modify{id = "top-center-anchor", visible = visible} --
+                :modify{id = "top-right-anchor", visible = visible} --
+                -- :newrow() --
+                :modify{id = "middle-left-anchor", visible = visible} --
+                :modify{id = "middle-center-anchor", visible = visible} --
+                :modify{id = "middle-right-anchor", visible = visible} --
+                -- :newrow() --
+                :modify{id = "bottom-left-anchor", visible = visible} --
+                :modify{id = "bottom-center-anchor", visible = visible} --
+                :modify{id = "bottom-right-anchor", visible = visible} --
+            end
+
             dialog --
             :separator{text = "Tracked"} --
             :combobox{
                 id = "trackedLayer",
                 label = "Layer",
-                options = layerNames
+                options = layerNames,
+                onchange = function() updateAnchors() end
             } --
             :combobox{
                 id = "framesOption",
                 label = "Frames:",
-                options = framesOptions
+                options = framesOptions,
+                onchange = function() updateAnchors() end
             } --
-            :separator{text = "Anchor"} --
-            :button{text = "X"} --
-            :button{text = ""} --
-            :button{text = ""} --
+            :separator{id = "anchor-separator", text = "Anchor"} --
+            :button{id = "top-left-anchor", text = "X"} --
+            :button{id = "top-center-anchor", text = ""} --
+            :button{id = "top-right-anchor", text = ""} --
             :newrow() --
-            :button{text = ""} --
-            :button{text = ""} --
-            :button{text = ""} --
+            :button{id = "middle-left-anchor", text = ""} --
+            :button{id = "middle-center-anchor", text = ""} --
+            :button{id = "middle-right-anchor", text = ""} --
             :newrow() --
-            :button{text = ""} --
-            :button{text = ""} --
-            :button{text = ""} --
+            :button{id = "bottom-left-anchor", text = ""} --
+            :button{id = "bottom-center-anchor", text = ""} --
+            :button{id = "bottom-right-anchor", text = ""} --
             :separator() --
             :button{
                 text = "OK",
                 onclick = function()
                     local trackedLayer = layers[dialog.data.trackedLayer]
-                    local framesOption = dialog.data["framesOption"]
-
-                    local framesRange = {
-                        fromFrame = 1,
-                        toFrame = #sprite.frames
-                    }
-
-                    if framesOption == AllFrames then
-                        -- Nothing specific
-                    elseif framesOption == SpecificFrames then
-                        -- TODO: Implement
-                    else -- Tag
-                        local tagName = string.sub(framesOption,
-                                                   #TagFramesPrefix + 1)
-
-                        for _, tag in ipairs(sprite.tags) do
-                            if tag.name == tagName then
-                                framesRange = {
-                                    fromFrame = tag.fromFrame.frameNumber,
-                                    toFrame = tag.toFrame.frameNumber
-                                }
-                                break
-                            end
-                        end
-                    end
+                    local framesRange = getSelectedFramesRange()
 
                     app.transaction(function()
                         TrackCels(sprite, trackedLayer, framesRange)
@@ -167,6 +211,9 @@ function init(plugin)
                 end
             } --
             :button{text = "Cancel"}
+
+            -- Initialize anchors
+            updateAnchors()
 
             dialog:show()
         end
