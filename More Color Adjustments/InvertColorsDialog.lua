2b7da539172e724c536dabb7ef2937447a53c777
Kacper Woźniak <kac.wozniak@gmail.com>
2023-09-23 20:45:47 +0200

    Add options for inverting multiple components

--- More Color Adjustments/InvertColorsDialog.lua
+++ More Color Adjustments/InvertColorsDialog.lua
@@ -3,139 +3,61 @@ local ColorConverter = dofile('./ColorConverter.lua')
 local GetPixels = dofile('./ImagePixels.lua')
 local PreviewCanvas = dofile('./PreviewCanvas.lua')
 
-local InvertColorSpace = {HSV = "HSV/HSL", OKHSV = "OKHSV/OKHSL"}
-local InvertMode = {Hue = "Hue", Saturation = "Saturation", Value = "Value"}
+local InvertColorSpace = {
+    RGB = "RGB",
+    HSV = "HSV",
+    HSL = "HSL",
+    OKHSV = "OKHSV",
+    OKHSL = "OKHSL"
+}
 
-function LerpColor(a, b, tt)
-    return Color {
-        hue = a.hue * (1 - tt) + b.hue * tt,
-        saturation = a.saturation * (1 - tt) + b.saturation * tt,
-        value = a.value * (1 - tt) + b.value * tt,
-        alpha = 255
-    }
-end
-
-function GetLight(pixelColor)
-    return pixelColor.red * 0.3 + pixelColor.green * 0.59 + pixelColor.blue *
-               0.11
-end
-
-function DarkenColor(c, v)
-    local l = GetLight(c) -- current light
-    -- v -- expected light
-    local d = v / l
-
-    local nc = Color(c)
-
-    while true do
-        local nl = GetLight(nc)
-
-        if math.abs(nl - v) <= 3 then break end
-
-        if nl > v then
-            nc.hslLightness = nc.hslLightness - 0.01
-        else
-            nc.hslLightness = nc.hslLightness + 0.01
-        end
-
-        if nc.hslLightness <= 0 or nc.hslLightness >= 1 then break end
-
-    end
-
-    return nc
-end
-
-local InvertCelHSV = function(cel, mode)
-    if not cel.layer.isEditable then return end
-
-    local selection = cel.sprite.selection
-    local image = cel.image
-    local getPixel, drawPixel = image.getPixel, image.drawPixel
-
-    local resultImage = Image(image)
-
-    local cache = {}
-
-    for x = 0, image.width - 1 do
-        for y = 0, image.height - 1 do
-            if selection.isEmpty or
-                selection:contains(cel.position.x + x, cel.position.y + y) then
-
-                local pixelValue = getPixel(image, x, y)
-
-                if pixelValue > 0 then
-                    local pixelColor = Color(pixelValue)
-
-                    if mode == InvertMode.Hue then
-
-                        if pixelColor.hslSaturation > 0 then
-                            local key = tostring(pixelColor.rgbaPixel)
-
-                            if cache[key] then
-                                pixelColor = Color(cache[key])
-                            else
-                                local o = GetLight(pixelColor)
-
-                                pixelColor.hslHue =
-                                    (pixelColor.hslHue + 180) % 360
-
-                                local maxColor = Color(pixelColor)
-                                -- maxColor.hslLightness = 1.0
-                                maxColor.value = 1.0
-
-                                local minColor = Color(pixelColor)
-                                -- minColor.hslLightness = 0.0
-                                minColor.value = 0.0
-
-                                local maxLum = GetLight(maxColor)
-                                local minLum = GetLight(minColor)
-
-                                print(minColor.red, minColor.green,
-                                      maxColor.red, maxColor.green, minLum,
-                                      maxLum)
-
-                                pixelColor =
-                                    LerpColor(minColor, maxColor, -- o / 255)
-                                    (o - minLum) / (maxLum - minLum))
-
-                                cache[key] = Color(pixelColor)
-                            end
-                        end
-                    elseif mode == InvertMode.Saturation then
-                        -- Ignore pixels in grayscale, they always have hue=0 so they would always end up red
-                        if pixelColor.saturation > 0 then
-                            pixelColor.hslSaturation = 1 -
-                                                           pixelColor.hslSaturation
-                        end
-                    elseif mode == InvertMode.Value then
-                        pixelColor.hslLightness = 1 - pixelColor.hslLightness
-                    end
-
-                    drawPixel(resultImage, x, y, pixelColor)
-                end
-            end
-        end
-    end
-
-    -- Update only the image to preserve cel properties
-    cel.image = resultImage
-end
-
-local InvertColor = function(pixelValue, colorSpace)
+local InvertColor = function(pixelValue, parameters)
     local color = Color(pixelValue)
 
-    if colorSpace == InvertColorSpace.HSV then
-        color.hue = (color.hue + 180) % 360
-    elseif colorSpace == InvertColorSpace.OKHSV then
+    if parameters.colorSpace == InvertColorSpace.RGB then
+        if parameters.componentA then color.red = 255 - color.red end
+        if parameters.componentB then color.green = 255 - color.green end
+        if parameters.componentC then color.blue = 255 - color.blue end
+    elseif parameters.colorSpace == InvertColorSpace.HSV then
+        if parameters.componentA then
+            color.hsvHue = (color.hsvHue + 180) % 360
+        end
+        if parameters.componentB then
+            color.hsvSaturation = 1 - color.hsvSaturation
+        end
+        if parameters.componentC then color.hsvValue = 1 - color.hsvValue end
+    elseif parameters.colorSpace == InvertColorSpace.HSL then
+        if parameters.componentA then
+            color.hslHue = (color.hslHue + 180) % 360
+        end
+        if parameters.componentB then
+            color.hslSaturation = 1 - color.hslSaturation
+        end
+        if parameters.componentC then
+            color.hslLightness = 1 - color.hslLightness
+        end
+    elseif parameters.colorSpace == InvertColorSpace.OKHSV then
         local okhsv = ColorConverter:ColorToOkhsv(color)
-        okhsv.h = (okhsv.h + 180) % 360
+
+        if parameters.componentA then okhsv.h = (okhsv.h + 180) % 360 end
+        if parameters.componentB then okhsv.s = 1 - okhsv.s end
+        if parameters.componentC then okhsv.v = 1 - okhsv.v end
+
         color = ColorConverter:OkhsvToColor(okhsv)
+    elseif parameters.colorSpace == InvertColorSpace.OKHSL then
+        local okhsl = ColorConverter:ColorToOkhsl(color)
+
+        if parameters.componentA then okhsl.h = (okhsl.h + 180) % 360 end
+        if parameters.componentB then okhsl.s = 1 - okhsl.s end
+        if parameters.componentC then okhsl.l = 1 - okhsl.l end
+
+        color = ColorConverter:OkhslToColor(okhsl)
     end
 
     return color
 end
 
-local InvertCelColors = function(cel, colorSpace)
+local InvertCelColors = function(cel, parameters)
     local selection = cel.sprite.selection
     local image = cel.image
     local getPixel, drawPixel = image.getPixel, image.drawPixel
@@ -148,21 +70,14 @@ local InvertCelColors = function(cel, colorSpace)
         for y = 0, image.height - 1 do
             if selection.isEmpty or
                 selection:contains(cel.position.x + x, cel.position.y + y) then
-
                 local pixelValue = getPixel(image, x, y)
 
                 if pixelValue > 0 then
-                    local key = tostring(pixelValue)
-
-                    if cache[key] then
-                        pixelValue = Color(cache[key])
-                    else
-                        pixelValue =
-                            InvertColor(pixelValue, colorSpace).rgbaPixel
-                        cache[key] = Color(pixelValue)
+                    if not cache[pixelValue] then
+                        cache[pixelValue] = InvertColor(pixelValue, parameters)
                     end
 
-                    drawPixel(resultImage, x, y, pixelValue)
+                    drawPixel(resultImage, x, y, cache[pixelValue])
                 end
             end
         end
@@ -172,12 +87,12 @@ local InvertCelColors = function(cel, colorSpace)
     cel.image = resultImage
 end
 
-local InvertColors = function(colorSpace)
+local InvertColors = function(parameters)
     app.transaction(function()
         for _, cel in ipairs(app.range.cels) do
             -- Invert colors only for the editable cels
             if cel.layer.isEditable then
-                InvertCelColors(cel, colorSpace)
+                InvertCelColors(cel, parameters)
             end
         end
     end)
@@ -192,8 +107,7 @@ local InvertPixels = function(image, pixels, parameters)
     for _, pixel in ipairs(pixels) do
         if pixel.isEditable then
             if not cache[pixel.value] then
-                cache[pixel.value] = InvertColor(pixel.value,
-                                                 parameters.colorSpace)
+                cache[pixel.value] = InvertColor(pixel.value, parameters)
             end
 
             drawPixel(image, pixel.x, pixel.y, cache[pixel.value])
@@ -208,38 +122,77 @@ end
 local InvertColorsDialog = function(sprite)
     local image, pixels = GetPixels(sprite)
     local invertedImage = Image(image)
-    invertedImage = InvertPixels(invertedImage, pixels,
-                                 {colorSpace = InvertColorSpace.OKHSV})
+    invertedImage = InvertPixels(invertedImage, pixels, {
+        colorSpace = InvertColorSpace.OKHSV,
+        componentA = true
+    })
 
     local dialog = Dialog("Invert Colors")
     local RepaintImage = PreviewCanvas(dialog, 100, 100, app.activeSprite,
                                        invertedImage)
+
+    local RefreshInvertedImage = function()
+        invertedImage = InvertPixels(invertedImage, pixels, dialog.data)
+        RepaintImage(invertedImage)
+    end
+
+    local RefreshComponentLabels = function(colorSpace)
+        if colorSpace == InvertColorSpace.RGB then
+            dialog --
+            :modify{id = "componentA", label = "Red"} --
+            :modify{id = "componentB", label = "Green"} --
+            :modify{id = "componentC", label = "Blue"} --
+        elseif colorSpace == InvertColorSpace.HSV or colorSpace ==
+            InvertColorSpace.OKHSV then
+            dialog --
+            :modify{id = "componentA", label = "Hue"} --
+            :modify{id = "componentB", label = "Saturation"} --
+            :modify{id = "componentC", label = "Value"} --
+        elseif colorSpace == InvertColorSpace.HSL or colorSpace ==
+            InvertColorSpace.OKHSL then
+            dialog --
+            :modify{id = "componentA", label = "Hue"} --
+            :modify{id = "componentB", label = "Saturation"} --
+            :modify{id = "componentC", label = "Lightness"} --
+        end
+    end
 
     dialog --
     :combobox{
         id = "colorSpace",
         label = "Color Space:",
         option = InvertColorSpace.OKHSV,
-        options = {InvertColorSpace.HSV, InvertColorSpace.OKHSV},
+        options = {
+            InvertColorSpace.RGB, InvertColorSpace.HSV, InvertColorSpace.HSL,
+            InvertColorSpace.OKHSV, InvertColorSpace.OKHSL
+        },
         onchange = function()
-            invertedImage = InvertPixels(invertedImage, pixels, dialog.data)
-            RepaintImage(invertedImage)
+            RefreshComponentLabels(dialog.data.colorSpace)
+            RefreshInvertedImage()
         end
     } --
+    :separator{text = "Components to Invert:"} --
+    :check{
+        id = "componentA",
+        label = "A",
+        selected = true,
+        onclick = RefreshInvertedImage
+    } --
+    :check{id = "componentB", label = "B", onclick = RefreshInvertedImage} --
+    :check{id = "componentC", label = "C", onclick = RefreshInvertedImage} --
     :separator() --
     :button{
         text = "&OK",
         onclick = function()
-            InvertColors(dialog.data.colorSpace)
+            InvertColors(dialog.data)
             dialog:close()
         end
     } --
     :button{text = "&Cancel"}
 
+    RefreshComponentLabels(InvertColorSpace.OKHSV)
+
     return dialog
 end
 
 return InvertColorsDialog
-
--- TODO: Add options for inverting multiple components
--- TODO: Add RGB
