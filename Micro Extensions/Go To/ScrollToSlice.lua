8b97f8f69900dc223131dd24babd1df10ddb1b05
Kacper Woźniak <kac.wozniak@gmail.com>
2024-11-24 15:24:26 +0100

    Fix scrolling to a slice for API version >=30

--- Micro Extensions/Go To/ScrollToSlice.lua
+++ Micro Extensions/Go To/ScrollToSlice.lua
@@ -1,9 +1,81 @@
-local Sizes = {
-    6400, 4800, 3200, 2400, 1600, 1200, 800, 600, 500, 400, 300, 200, 100, 50,
-    33.3, 25, 20, 16.7, 12.5
+local function ZoomValue(fraction, percentage)
+    return {fraction = fraction, percentage = percentage}
+end
+
+local ZoomValues = {
+    ZoomValue(64, "6400"), ZoomValue(48, "4800"), ZoomValue(32, "3200"),
+    ZoomValue(24, "2400"), ZoomValue(16, "1600"), ZoomValue(12, "1200"),
+    ZoomValue(8, "800"), ZoomValue(6, "600"), ZoomValue(5, "500"),
+    ZoomValue(4, "400"), ZoomValue(3, "300"), ZoomValue(2, "200"),
+    ZoomValue(1, "100"), ZoomValue(0.5, "50"), ZoomValue(0.333, "33.3"),
+    ZoomValue(0.25, "25"), ZoomValue(0.20, "20"), ZoomValue(0.167, "16.7"),
+    ZoomValue(0.125, "12.5")
 }
 
+local function ZoomOnBounds(bounds)
+    local width = bounds.width
+    local height = bounds.height
+    local maxWidth = app.window.width * 0.8
+    local maxHeight = app.window.height * 0.6
+
+    for _, zoom in ipairs(ZoomValues) do
+        local fraction = zoom.fraction
+        if (width * fraction < maxWidth) and (height * fraction < maxHeight) then
+            app.command.Zoom {percentage = zoom.percentage, focus = "center"}
+            break
+        end
+    end
+end
+
+local function ScrollToSliceV30(slice, autoZoom)
+    local bounds = slice.bounds
+
+    -- Avoid using "zoomed-pixel" as its doesnt't work if zoom is less than 100%
+    local zoom = app.editor.zoom
+
+    local editorScrollX = app.editor.scroll.x
+    local editorScrollY = app.editor.scroll.y
+
+    local sliceCenterX = bounds.x + bounds.width / 2
+    local sliceCenterY = bounds.y + bounds.height / 2
+
+    if sliceCenterX < editorScrollX then
+        app.command.Scroll {
+            direction = "left",
+            units = "pixel",
+            quantity = tostring((editorScrollX - sliceCenterX) * zoom)
+        }
+    else
+        app.command.Scroll {
+            direction = "right",
+            units = "pixel",
+            quantity = tostring((sliceCenterX - editorScrollX) * zoom)
+        }
+    end
+
+    if sliceCenterY < editorScrollY then
+        app.command.Scroll {
+            direction = "up",
+            units = "pixel",
+            quantity = tostring((editorScrollY - sliceCenterY) * zoom)
+        }
+    else
+        app.command.Scroll {
+            direction = "down",
+            units = "pixel",
+            quantity = tostring((sliceCenterY - editorScrollY) * zoom)
+        }
+    end
+
+    if autoZoom then ZoomOnBounds(bounds) end
+end
+
 return function(slice, autoZoom)
+    if app.apiVersion >= 30 then
+        ScrollToSliceV30(slice, autoZoom)
+        return
+    end
+
     local bounds = slice.bounds
 
     -- If auto zooming reset the zoom first to avoid using "zoomed-pixel" units
@@ -50,18 +122,5 @@ return function(slice, autoZoom)
         }
     end
 
-    if autoZoom then
-        local maxWidth, maxHeight = app.window.width * 0.8,
-                                    app.window.height * 0.6
-
-        for _, size in ipairs(Sizes) do
-            local sizeFraction = size / 100
-
-            if (bounds.width * sizeFraction < maxWidth) and
-                (bounds.height * sizeFraction < maxHeight) then
-                app.command.Zoom {percentage = tostring(size), focus = "center"}
-                break
-            end
-        end
-    end
+    if autoZoom then ZoomOnBounds(bounds) end
 end
