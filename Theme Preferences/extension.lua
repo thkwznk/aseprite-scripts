46cf39a0bb40b201a4fd2b2f4da3a7a57b7c947a
Kacper Woźniak <kac.wozniak@gmail.com>
2026-02-15 01:04:05 +0100

    Refactor the functions to update theme files to a separate file

--- Theme Preferences/extension.lua
+++ Theme Preferences/extension.lua
@@ -1,59 +1,16 @@
 local Template = dofile("./Template.lua")
 local ThemeManager = dofile("./ThemeManager.lua")
+local CopyColor = dofile("./CopyColor.lua")
+local UpdateThemeFiles = dofile("./UpdateThemeFiles.lua")
 
 local THEME_ID = "custom"
 local DIALOG_WIDTH = 240
 local DIALOG_TITLE = "Theme Preferences"
 
-local ExtensionsDirectory = app.fs.joinPath(app.fs.userConfigPath, "extensions")
-local ThemePreferencesDirectory = app.fs.joinPath(ExtensionsDirectory,
-                                                  "theme-preferences")
-local SheetTemplatePath = app.fs.joinPath(ThemePreferencesDirectory,
-                                          "sheet-template.png")
-local SheetPath = app.fs.joinPath(ThemePreferencesDirectory, "sheet.png")
-local ThemeXmlTemplatePath = app.fs.joinPath(ThemePreferencesDirectory,
-                                             "theme-template.xml")
-local ThemeXmlPath = app.fs.joinPath(ThemePreferencesDirectory, "theme.xml")
-
-local function ReadAll(filePath)
-    local file = assert(io.open(filePath, "rb"))
-    local content = file:read("*all")
-    file:close()
-    return content
-end
-
-local function WriteAll(filePath, content)
-    local file = io.open(filePath, "w")
-    if file then
-        file:write(content)
-        file:close()
-    end
-end
-
-local function ColorToHex(color)
-    return string.format("#%02x%02x%02x", color.red, color.green, color.blue)
-end
-
-local function RgbaPixelToColor(rgbaPixel)
-    return Color {
-        red = app.pixelColor.rgbaR(rgbaPixel),
-        green = app.pixelColor.rgbaG(rgbaPixel),
-        blue = app.pixelColor.rgbaB(rgbaPixel),
-        alpha = app.pixelColor.rgbaA(rgbaPixel)
-    }
-end
-
-local function CopyColor(originalColor)
-    return Color {
-        red = originalColor.red,
-        green = originalColor.green,
-        blue = originalColor.blue,
-        alpha = originalColor.alpha
-    }
-end
+local max, min = math.max, math.min
 
 local function ShiftRGB(value, modifier)
-    return math.max(math.min(value + modifier, 255), 0)
+    return max(min(value + modifier, 255), 0)
 end
 
 local function ShiftColor(color, redModifier, greenModifer, blueModifier)
@@ -77,77 +34,8 @@ for id, parameter in pairs(Template.parameters) do
     Theme.parameters[id] = parameter
 end
 
-local function UpdateThemeXml(theme)
-    -- Prepare theme.xml
-    local xmlContent = ReadAll(ThemeXmlTemplatePath)
-
-    for id, color in pairs(theme.colors) do
-        xmlContent = xmlContent:gsub("<" .. id .. ">", ColorToHex(color))
-    end
-
-    WriteAll(ThemeXmlPath, xmlContent)
-end
-
-local function RefreshTheme(template, theme)
-    -- Prepare color lookup
-    local Map = {}
-
-    for id, templateColor in pairs(template.colors) do
-        -- Map the template color to the theme color
-        Map[ColorToHex(templateColor)] = theme.colors[id]
-    end
-
-    -- Prepare sheet.png
-    local image = Image {fromFile = SheetTemplatePath}
-    local pixelValue, newColor, pixelData, pixelColor, pixelValueKey,
-          resultColor
-
-    -- Save references to function to improve performance
-    local getPixel, drawPixel = image.getPixel, image.drawPixel
-
-    local cache = {}
-
-    for x = 0, image.width - 1 do
-        for y = 0, image.height - 1 do
-            pixelValue = getPixel(image, x, y)
-
-            if pixelValue > 0 then
-                pixelValueKey = tostring(pixelValue)
-                pixelData = cache[pixelValueKey]
-
-                if not pixelData then
-                    pixelColor = RgbaPixelToColor(pixelValue)
-
-                    cache[pixelValueKey] = {
-                        id = ColorToHex(pixelColor),
-                        color = pixelColor
-                    }
-
-                    pixelData = cache[pixelValueKey]
-                end
-
-                resultColor = Map[pixelData.id]
-
-                if resultColor ~= nil then
-                    newColor = CopyColor(resultColor)
-                    newColor.alpha = pixelData.color.alpha -- Restore the original alpha value
-
-                    drawPixel(image, x, y, newColor)
-                end
-            end
-        end
-    end
-
-    image:saveAs(SheetPath)
-
-    -- Update the XML theme file
-    UpdateThemeXml(theme)
-
-    app.command.Refresh()
-end
-
 local function ApplyTheme()
-    RefreshTheme(Template, Theme)
+    UpdateThemeFiles(Template, Theme)
     ThemeManager:SetCurrentTheme(Theme)
 
     -- Switch Aseprite to the custom theme
